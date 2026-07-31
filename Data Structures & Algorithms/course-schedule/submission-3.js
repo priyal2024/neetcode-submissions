9d273dbd3843b71a30d5088beb7eb8b3dd2cdbd7
priyal2024 <36183217+priyal2024@users.noreply.github.com>
2026-07-31 12:47:17 +0100

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let adj = new Map();
+        let indegree = new Array(numCourses).fill(0);
+        let q = [];
+        let finish = 0;
+
+        for(let i=0; i<numCourses; i++){
+            adj.set(i, []);
+        }
+
+        for(const [crs, pre] of prerequisites){
+            indegree[pre]++;
+            adj.get(crs).push(pre);
+        }
+
+        for(let i=0; i<numCourses; i++){
+            if(indegree[i] === 0){
+                q.push(i);
+            }
+        }
+
+        while(q.length){
+            let node = q.shift();
+            finish++;
+            
+            for(let nei of adj.get(node)){
+                indegree[nei]--;
+                if(indegree[nei] === 0){
+                    q.push(nei);
+                }
+            }
+        }
+        return finish === numCourses;
+    }
+}
